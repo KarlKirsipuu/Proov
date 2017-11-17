51a2cce89624b66ad225079746a595c813ad237d
Karl Kirsipuu <karl_kirsipuu@hot.ee>
2017-11-17 13:51:22 +0200

    proov

---
+++ rakendus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnitöö_rakendus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("See on siin nüüd mingisugune programm.");
+            Console.ReadLine();
+
+        }
+    }
+}
